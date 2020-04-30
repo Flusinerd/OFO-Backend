7f7466b762d26bb8f8b819f40e6aef715917773d
Jan Krüger <jankrueger1999@gmail.com>
2020-04-30 18:31:41 +0200

    Matched mock with service

--- test/event-user.service.mock.ts
+++ test/event-user.service.mock.ts
@@ -14,10 +14,6 @@ export class EventUserServiceMock {
     return [new EventUserMock()];
   }
 
-  async getAllOfEvent(event: EventEntity | number): Promise<EventUserEntity[]> {
-    return [new EventUserMock()];
-  }
-
   async createOne(data: EventUserInput): Promise<EventUserEntity> {
     return new EventUserMock();
   }
